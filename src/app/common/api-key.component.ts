bb9b7b6edb1783f78705f6ee969b7a936e009845
Amber <amber.leblanc@cra-arc.gc.ca>
2025-06-25 23:18:05 -0400

    Update button style

--- src/app/common/api-key.component.ts
+++ src/app/common/api-key.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule } from "@ngx-translate/core";
 import { LocalStorageService } from '../services/local-storage.service';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
 
 @Component({
   selector: 'ca-api-key',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, TranslateModule, ButtonModule, InputGroupModule],
   templateUrl: './api-key.component.html',
   styles: ``
 })
 export class ApiKeyComponent {
+  api: string = '';
+  error: string = '';
+
   saveAPI(value: string) {
         this.localStore.saveData('apiKey', value);
         console.log('The saved API key is: ', this.localStore.getData('apiKey'));
        }
-  removeAPI(value: string) {
+  removeAPI() {
         this.localStore.removeData('apiKey');
         console.log('The saved API key is: ', this.localStore.getData('apiKey'));
          
